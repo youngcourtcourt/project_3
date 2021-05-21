d85caa18a85107a7c03f44743de878b76fce4f42
youngcourtcourt <courtneymacdonald@Courtneys-iMac.local>
2021-05-21 11:14:07 -0700

    looping through csv

--- static/js/readCsv.js
+++ static/js/readCsv.js
@@ -2,4 +2,10 @@ d3.csv("../data/aggregateCountyData.csv").then(function(data){
     
     console.log(data)
 
+    for (var i=0;i<data.length;i++){
+        // console.log((data[i]['County Name']).replaceAll('_', ' '))
+
+        county=(data[i]['County Name']).replaceAll('_', ' ')
+    }
+
 })
